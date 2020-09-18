24c913a97f707c2d726ce1374f791f2dce930094
FSL-Tikaani <70714295+FSL-Tikaani@users.noreply.github.com>
2020-09-18 23:40:54 +0300

    calculator

--- JAVA/src/com/company/Main.java
+++ JAVA/src/com/company/Main.java
@@ -1,15 +1,38 @@
 package com.company;
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String scString = sc.nextLine();
-        String[] scStrings = scString.split(" ");
-        int num1 = Integer.parseInt(scStrings[0]);
-        int num2 = Integer.parseInt(scStrings[1]);
-        System.out.println(num1 + num2);
+        Scanner cons = new Scanner(System.in);
+        int num1 = cons.nextInt();
+        int num2 = cons.nextInt();
+        int otvet;
+
+        char operation = cons.next().charAt(0);
+
+        switch (operation) {
+            case '+':
+                otvet = num1+num2;
+                System.out.println(num1 + " + " + num2 + " = " + otvet);
+                break;
+
+            case '-':
+                otvet = num1-num2;
+                System.out.println(num1 + " - " + num2 + " = " + otvet);
+                break;
+
+            case '*':
+                otvet = num1*num2;
+                System.out.println(num1 + " * " + num2 + " = " + otvet);
+                break;
+
+            case '/':
+                otvet = num1/num2;
+                System.out.println(num1 + " / " + num2 + " = " + otvet);
+                break;
+        }
 
 
     }
